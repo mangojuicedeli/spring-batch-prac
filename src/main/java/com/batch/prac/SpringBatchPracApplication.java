804357769573f20687717d71faa4a437bb0bc0b4
mangojuicedeli <mgkang0317@gmail.com>
2020-08-20 20:18:35 +0900

    initial commit

---
+++ src/main/java/com/batch/prac/SpringBatchPracApplication.java
@@ -0,0 +1,13 @@
+package com.batch.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/prac/SpringBatchPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
