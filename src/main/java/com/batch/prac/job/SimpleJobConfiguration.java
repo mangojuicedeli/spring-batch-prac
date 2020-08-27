77b1dacf5d4d88eac527e95fecf3f5e2451b7fef
mangojuicedeli <mgkang0317@gmail.com>
2020-08-27 15:51:55 +0900

    step의 tasklet을 클래스로 따로 빼내서 job parameter scope 테스트

--- src/main/java/com/batch/prac/job/SimpleJobConfiguration.java
+++ src/main/java/com/batch/prac/job/SimpleJobConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,35 +21,33 @@ public class SimpleJobConfiguration {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
+	private final SimpleJobTasklet tasklet1;
 	
 	@Bean
 	public Job simpleJob() {
 		return jobBuilderFactory
 				.get("simpleJob")
-				.start(simpleStep1(null))
+				.start(simpleStep1())
 				.next(simpleStep2(null))
 				.build();
 	}
 
 	@Bean
 	@JobScope
-	public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
-		return stepBuilderFactory.get("simpleStep1").tasklet(
-				(contribution, chuckContext) -> {
-					log.info(">>>> this is step1");
-					log.info(">>>> requestDate = {}", requestDate);
-					return RepeatStatus.FINISHED;
-				}).build();
+	public Step simpleStep1() {
+		log.info(">>>>> this is step1");
+		
+		return stepBuilderFactory.get("simpleStep1").tasklet(tasklet1).build();
 	}
 	
 	@Bean
 	@JobScope
 	public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+		log.info(">>>> this is step2");
 		
 		return stepBuilderFactory.get("simpleStep2").tasklet(
 				(contribution, chuckContext) -> {
-					log.info(">>>> this is step2");
-					log.info(">>>> requestDate = {}", requestDate);
+					log.info(">>>>> requestDate = {}", requestDate);
 					return RepeatStatus.FINISHED;
 				}).build();
 	}
