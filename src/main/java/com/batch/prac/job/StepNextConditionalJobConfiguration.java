d068e36bd7e8a65e913d697974dfc3caae34685e
mangojuicedeli <mgkang0317@gmail.com>
2020-08-27 14:41:46 +0900

    분기 작업을 decider 커스텀 클래스가 맡도록 리팩토링

--- src/main/java/com/batch/prac/job/StepNextConditionalJobConfiguration.java
+++ src/main/java/com/batch/prac/job/StepNextConditionalJobConfiguration.java
@@ -12,6 +12,10 @@ import org.springframework.context.annotation.Configuration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+/*
+ * next()의 경우 다음 step으로 순차적으로 일이 진행되었는데,
+ * 그 전 step의 결과에 따라 다음 step이 분기가 되도록 할 수 있다.
+ */
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
@@ -24,32 +28,33 @@ public class StepNextConditionalJobConfiguration {
 	public Job stepNextConditionalJob() {
 		return jobBuilderFactory.get("stepNextConditionalJob")
 				.start(conditionalJobStep1())
-				.on("FAILED") // FAILED 일 경우
-					.to(conditionalJobStep3()) // step3으로 이동한다.
+					.on("FAILED") // FAILED 일 경우
+						.to(conditionalJobStep3()) // step3으로 이동한다.
 					.on("*") // step3의 결과 관계 없이
-					.end() // step3으로 이동하면 Flow가 종료한다.
+						.end() // step3으로 이동하면 Flow가 종료한다.
 				.from(conditionalJobStep1()) // step1로부터
 					.on("*") // FAILED 외에 모든 경우
-					.to(conditionalJobStep2()) // step2로 이동한다.
-					.next(conditionalJobStep3()) // step2가 정상 종료되면 step3으로 이동한다.
+						.to(conditionalJobStep2()) // step2로 이동한다.
+						.next(conditionalJobStep3()) // step2가 정상 종료되면 step3으로 이동한다.
 					.on("*") // step3의 결과 관계 없이
-					.end() // step3으로 이동하면 Flow가 종료한다.
+						.end() // step3으로 이동하면 Flow가 종료한다.
 				.end() // Job 종료
 				.build();
 	}
 
 	@Bean
 	public Step conditionalJobStep1() {
-		return stepBuilderFactory.get("step1").tasklet((contribution, chunkContext) -> {
-			log.info(">>>>> This is stepNextConditionalJob Step1");
-
-			/**
-			 * ExitStatus를 FAILED로 지정한다. 해당 status를 보고 flow가 진행된다.
-			 **/
-			contribution.setExitStatus(ExitStatus.FAILED);
-
-			return RepeatStatus.FINISHED;
-		}).build();
+		return stepBuilderFactory.get("step1")
+				.tasklet((contribution, chunkContext) -> {
+					log.info(">>>>> This is stepNextConditionalJob Step1");
+		
+					/**
+					 * ExitStatus를 FAILED로 지정한다. 해당 status를 보고 flow가 진행된다.
+					 **/
+					contribution.setExitStatus(ExitStatus.FAILED);
+		
+					return RepeatStatus.FINISHED;
+				}).build();
 	}
 
 	@Bean
